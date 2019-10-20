1f7903dabdbcfc9a1e69330616f75adacfa9b91f
geraldy <geraldy.leondas@gmail.com>
2019-10-20 19:21:30 +0200

    Updates

---
+++ precache-manifest.95e415432fc42eedbb5033927356f996.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c1056a1b8bc928e3475023b3cd2725f",
+    "url": "/unrepos/index.html"
+  },
+  {
+    "revision": "dbca6580c7057d526b60",
+    "url": "/unrepos/static/css/main.cc0b18ba.chunk.css"
+  },
+  {
+    "revision": "b0703eff4582a78f493a",
+    "url": "/unrepos/static/js/2.5075ebff.chunk.js"
+  },
+  {
+    "revision": "dbca6580c7057d526b60",
+    "url": "/unrepos/static/js/main.cbd645cd.chunk.js"
+  },
+  {
+    "revision": "07246b8a5d98da5ab6a8",
+    "url": "/unrepos/static/js/runtime-main.454bd026.js"
+  }
+]);
